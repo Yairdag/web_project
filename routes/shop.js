e9391283ce390ec5a5ba054295aef7c8bdd8a4b4
Elazar <elazarbsh@gmail.com>
2022-03-04 21:31:26 +0200

    added edit product and delete product functionality and a display orders page

--- routes/shop.js
+++ routes/shop.js
@@ -4,37 +4,17 @@ const User = require("../models/user");
 
 const router = require('express').Router();
 
-products = [{
-    name: "laptop",
-    price: 400,
-    imgUrl: "a",
-    id: 1
-}, {
-    name: "phone",
-    price: 200,
-    imgUrl: "a",
-    id: 2
-}];
 
 router.get("/products", (req, res) => {
     Product.find()
-    .then(products => {
-      res.render("products", { products: products, user: req.user });
-    })
-    .catch(err => {
-      console.log(err);
-    });
+        .then(products => {
+            res.render("products", { products: products, user: req.user });
+        })
+        .catch(err => {
+            console.log(err);
+        });
 });
 
-
-
-// total_price: String,
-// items: [{item:String, amount:Number}]
-
-shopping_c = [{
-    total_price: "500",
-    items: [{ iphone12: 1 }, { air_pos_pro: 2 }]
-}];
 
 router.get("/shopping-cart", (req, res) => {
     res.render("shopping-cart", { cartProducts: req.user.cart.items, user: req.user });
@@ -59,6 +39,53 @@ router.get("/add-product", function (req, res) {
     res.render("admin/add-product");
 });
 
+router.get('/edit-product/:productId', function (req, res) {
+    console.log("hi");
+    const prodId = req.params.productId;
+    console.log(prodId);
+    Product.findById(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                product: product,
+            });
+        })
+        .catch(err => console.log(err));
+});
+
+router.post('/edit-product', function (req, res) {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.name;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+  
+    Product.findById(prodId)
+      .then(product => {
+        product.name = updatedTitle;
+        product.price = updatedPrice;
+        product.imageUrl = updatedImageUrl;
+        return product.save();
+      })
+      .then(result => {
+        console.log(result);
+        res.redirect('/products');
+      })
+      .catch(err => console.log(err));
+});
+
+router.post('/delete-product', function (req, res) {
+    const prodId = req.body.productId;
+    Product.findByIdAndRemove(prodId)
+      .then(() => {
+        console.log('DESTROYED PRODUCT');
+        res.redirect('/products');
+      })
+      .catch(err => console.log(err));
+});
+
+
 router.post("/cart", (req, res) => {
     const prodId = req.body.productId;
     console.log(prodId);
@@ -75,32 +102,41 @@ router.post("/cart", (req, res) => {
 
 router.post("/order", (req, res) => {
     User.findById(req.user.id)
-    .populate('cart.items.productId')
-    .then(user => {
-      const products = user.cart.items.map(i => {
-        return { quantity: i.quantity, product: { ...i.productId.toJSON() } };
-      });
-      const order = new Order({
-        products: products,
-        userId: req.user
-      });
-      return order.save();
-    })
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+        .populate('cart.items.productId')
+        .then(user => {
+            const products = user.cart.items.map(item => {
+                return { quantity: item.quantity, product: { ...item.productId.toJSON() } };
+            });
+            const order = new Order({
+                products: products,
+                userId: req.user
+            });
+            return order.save();
+        })
+        .then(() => {
+            res.redirect('/');
+        })
+        .catch(err => console.log(err));
 });
 
 router.get("/orders", (req, res) => {
     Order.find({ userId: req.user.id })
-    .then(orders => {
-      res.render('shop/orders', {
-        orders: orders,
-      });
-    })
-    .catch(err => console.log(err));
+        .then(orders => {
+            res.render('user-orders', {
+                orders: orders, user: req.user
+            });
+        })
+        .catch(err => console.log(err));
 });
 
+router.get("/all-orders", (req, res) => {
+    Order.find()
+        .then(orders => {
+            res.render('user-orders', {
+                orders: orders, user: req.user
+            });
+        })
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
